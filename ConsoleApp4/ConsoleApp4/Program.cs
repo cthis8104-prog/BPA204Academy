3962df5622d2affb5a53af6c90f719e636015408
cthis8104-prog <cthis8104@gmail.com>
2026-03-03 17:36:27 +0400

    lastValue

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string mehsul = "Hp";
+            int firstValue = 1000;
+            double percent = 0.11;
+            int lastValue = (int)(firstValue - (firstValue * percent));
+            Console.WriteLine($"mehsulun adi: {mehsul},son qiymet: {lastValue}");
+        }
+    }
+}
