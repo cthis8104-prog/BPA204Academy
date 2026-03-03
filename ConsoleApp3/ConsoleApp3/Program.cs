a3227136579396b671a4d0fdf36af31a32b34fb8
cthis8104-prog <cthis8104@gmail.com>
2026-03-03 17:29:06 +0400

    micro

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int sahe = (a * b) / 2;
+            Console.WriteLine(sahe);
+
+        }
+    }
+}
