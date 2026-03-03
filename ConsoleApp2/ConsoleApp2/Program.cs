c2776c73e7e0547dcd5256b0b79f42f9b3b7f337
cthis8104-prog <cthis8104@gmail.com>
2026-03-03 17:24:55 +0400

    main

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Salary1 = 1000;
+            int Salary2 = 1200;
+            int Salary3 = 1999;
+            int Salary4 = 1085;
+            int salaryOrta = (Salary1 + Salary2 + Salary3 + Salary4) / 4;
+            Console.WriteLine(salaryOrta);
+
+        }
+    }
+}
