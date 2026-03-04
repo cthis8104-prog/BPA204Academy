cc71ffacecc10dda63c48bac47926c50b456176d
cthis8104-prog <cthis8104@gmail.com>
2026-03-04 18:04:51 +0400

    04/03/2026 2

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  int personAge = int.Parse(Console.ReadLine());
+              if (personAge >= 65 && personAge<125 && personAge>0)
+              {
+                  Console.WriteLine("pensiya yasi catib");
+              }
+              else if(personAge<65 && personAge>0)
+              {
+                  Console.WriteLine("pensiya yasi catmayib");
+              }
+             else{
+                   Console.WriteLine("bele yas yoxdu");
+            }*/
+
+            /*int value = int.Parse(Console.ReadLine());
+            if (value % 2 == 0 && value>0)
+            {
+                Console.WriteLine("bu eded cutdur");
+
+            }
+            else if(value % 2 == 1 && value>0)
+            {
+                Console.WriteLine("bu eded tekdir");
+            }
+            else{
+              Console.WriteLine("bu eded menfidir");
+            }
+*/
+
+
+
+            /*    int value = int.Parse(Console.ReadLine());
+            if (value % 3== 0 && value % 5 == 0 && value>0)
+            {
+                Console.WriteLine("bu eded 3 ve 5 qaliqsiz bolunur");
+            }*/
+
+
+
+            /*        int value = int.Parse(Console.ReadLine());
+                    if (value % 2 == 0 && value>0 )
+                    {
+                        Console.WriteLine("bolundu");
+                    }
+                    else if(value % 2 == 1 && value > 0)
+                    {
+                        Console.WriteLine(value % 2);
+                    }
+                    else
+                    {
+                        Console.WriteLine("eded menfidir");
+                    }*/
+           /* int value1 = int.Parse(Console.ReadLine());
+            int value2 = int.Parse(Console.ReadLine());
+            if (value1 * value2 > 0)
+            {
+                Console.WriteLine("hasil musbetdir");
+            }
+            else
+            {
+                Console.WriteLine("hasil menfidir");
+            }*/
+
+        }
+    }
+}
