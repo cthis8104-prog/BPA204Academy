4533d18238964ee23ce82b6447630b91f1a7cb7c
cthis8104-prog <cthis8104@gmail.com>
2026-03-04 17:42:27 +0400

    04/03/2026

---
+++ 02-Task/02-Task/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+
+namespace _02_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+          
+            
+            
+            /*  for (int i = 1; i < 10; i++)
+              {
+                  Console.WriteLine(i);
+              }*/
+
+
+
+
+
+            /*   int a = 0;
+               for (int i = 1; i < 50; i++)
+               {
+                   a += i;
+               }
+               Console.WriteLine(a);*/
+
+            /*  for (int i = 9; i > 0; i++)
+              {
+                  Console.WriteLine(i);
+              }
+  */
+
+
+
+          
+            
+            
+            
+            /* int a = 0;
+         for (int i = 1; i < 20; i++)
+         {
+             if (i%2==0)
+             {
+
+             a += i;
+             }
+         }
+         Console.WriteLine(a);*/
+
+
+            /*
+                            int a = 0;
+                        for (int i = 1; i < 20; i++)
+                        {
+                            if (i % 2 == 1)
+                            {
+
+                                a += i;
+                            }
+                        }
+                        Console.WriteLine(a);
+
+            */
+
+
+
+          
+            
+            
+            
+            /*int a = 0;
+            int n = 0;
+            for (int i = 1; i < 100; i++)
+            {
+                a += i;
+                n++;
+            }
+            Console.WriteLine(a / n);*/
+
+            /*
+                        for (int i = 1; i < 100; i++)
+                        {
+                            if (i % 5 ==0 && i%7==0)
+                            {
+
+                                Console.WriteLine(i);
+                            }
+                        }*/
+
+
+          
+            
+            
+            
+            
+            /* int cemTek = 0;
+          
+            int cemCut = 0;
+             for (int i = 1; i < 10; i++)
+             {
+                 if (i%2==0)
+                 {
+                     cemCut += i;
+
+                 }
+                 else
+                 {
+                     cemTek += i;
+                 }
+
+             }
+                 Console.WriteLine(cemTek);
+                 Console.WriteLine(cemTek);
+ */
+          
+            
+            
+            
+            
+            
+            /*            int studentResult = int.Parse(Console.ReadLine());
+                        if (studentResult>=90 &&  studentResult<=100)
+
+                        {
+                            Console.WriteLine("A");
+
+                        }
+                        else if (studentResult >= 80 && studentResult < 90)
+                        {
+                            Console.WriteLine("B");
+
+                        }
+                        else if (studentResult >= 70 && studentResult < 80)
+                        {
+                            Console.WriteLine("C");
+
+                        }
+                        else if(studentResult<70)
+                        {
+                            Console.WriteLine("Ugursuz");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Bele proqnoz ola bilmez");
+                        }
+            */
+
+
+           
+            
+            
+            
+            
+            /*    int sum = 0;
+                int[] array = { 12, 14, 35, 56, 68, 93 };
+                for(int i=0;i<array.Length;i++)
+                {
+                    sum += array[i];
+                }
+                Console.WriteLine(sum);*/
+            /*        int sum = 0;
+                    int[] array = { 12, 14, 35, 56, 68, 93 };
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        if (i%2==0)
+                        {
+                        sum += array[i];
+
+                        }
+                    }
+
+
+
+
+
+
+
+
+          /*  int sum = 0;
+            int[] array = { 12, 14, 35, 56, 68, 93 };
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    sum += array[i];
+
+                }
+            }
+            Console.WriteLine(sum);*/
+        }
+    }
+}
