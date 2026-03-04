7296dfc66c53f63f09cd54007043887d11731fd6
cthis8104-prog <cthis8104@gmail.com>
2026-03-04 18:15:17 +0400

    04/03/2026 3

---
+++ myTask1/myTask1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace myTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* string name = Console.ReadLine();
+             string lastName = Console.ReadLine();
+             Console.WriteLine($"Full name: {name} {lastName}");*/
+
+/*
+            int height = int.Parse(Console.ReadLine());
+            int weight = int.Parse(Console.ReadLine());
+            int BMi = (height) / (weight * 2);
+            if (BMi >= 25)
+            {
+                Console.WriteLine("artiq cekili");
+
+            }
+            else if (BMi<25 && BMi>18.5)
+            {
+                Console.WriteLine("normal");
+            }
+            else
+            {
+                Console.WriteLine("ariq");
+            }
+*/
+
+
+
+
+
+
+        }
+    }
+}
